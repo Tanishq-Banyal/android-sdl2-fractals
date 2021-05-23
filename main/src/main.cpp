ab8f5bab59ae7beb13f8e69cc5f9a983abba4962
Tanishq-Banyal <68330937+Tanishq-Banyal@users.noreply.github.com>
2021-05-23 11:26:45 +0530

    Update main.cpp

--- main/src/main.cpp
+++ main/src/main.cpp
@@ -1,200 +1,224 @@
 #include "SDL.h"
-#include "SDL_image.h"
-#include "SDL_mixer.h"
-#include "SDL2_gfxPrimitives.h"
-#include "SDL_ttf.h"
 
-static char * icon_xpm[] = {
-        "32 23 3 1",
-        "     c #FFFFFF",
-        ".    c #000000",
-        "+    c #FFFF00",
-        "                                ",
-        "            ........            ",
-        "          ..++++++++..          ",
-        "         .++++++++++++.         ",
-        "        .++++++++++++++.        ",
-        "       .++++++++++++++++.       ",
-        "      .++++++++++++++++++.      ",
-        "      .+++....++++....+++.      ",
-        "     .++++.. .++++.. .++++.     ",
-        "     .++++....++++....++++.     ",
-        "     .++++++++++++++++++++.     ",
-        "     .++++++++++++++++++++.     ",
-        "     .+++++++++..+++++++++.     ",
-        "     .+++++++++..+++++++++.     ",
-        "     .++++++++++++++++++++.     ",
-        "      .++++++++++++++++++.      ",
-        "      .++...++++++++...++.      ",
-        "       .++............++.       ",
-        "        .++..........++.        ",
-        "         .+++......+++.         ",
-        "          ..++++++++..          ",
-        "            ........            ",
-        "                                "};
+#define _USE_MATH_DEFINES
+#define _CRT_SECURE_NO_WARNINGS
 
-int SDL_main(int argc, char* argv[]) {
+#include <cmath>
+#include <ctime>
+#include <cstdio>
+#include <cstdlib>
+#include <complex>
 
-    SDL_Window *window;                    // Declare a pointer
-    SDL_Surface *surface;
-    SDL_Texture *texture;
-    SDL_Init(SDL_INIT_VIDEO);              // Initialize SDL2
+using namespace std;
+using namespace complex_literals;
 
-    // Create an application window with the following settings:
-    window = SDL_CreateWindow(
-        "An SDL2 window",                  // window title
-        SDL_WINDOWPOS_UNDEFINED,           // initial x position
-        SDL_WINDOWPOS_UNDEFINED,           // initial y position
-        640,                               // width, in pixels
-        480,                               // height, in pixels
-        SDL_WINDOW_OPENGL                  // flags - see below
-    );
+#define WIDTH			720
+#define HEIGHT			1280
 
-    // Check that the window was successfully created
-    if (window == NULL) {
-        // In the case that the window could not be made...
-        printf("Could not create window: %s\n", SDL_GetError());
-        return 1;
-    }
+#define START_POS_X		0
+#define START_POS_Y		0
+#define START_ZOOM		(WIDTH * 0.2f)
 
-    if (Mix_OpenAudio(22050, MIX_DEFAULT_FORMAT, 2, 4096) == -1 ) {
-        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION,
-                     "Couldn't open mixer: %s", SDL_GetError());
-        return 2;
-    }
+#define BAIL_OUT		5
+#define STRIPS			30
 
-    Mix_Chunk *sample = Mix_LoadWAV("cuckoo.wav");
-    if (sample == NULL) {
-        fprintf(stderr, "Unable to load wave file\n");
-        return 3;
-    }
+#define ZOOM_FACTOR		1.5
+#define DETAILS			0.1
 
+#define SEED			2
+#define STRIP_BY_STRIP
 
-    // Setup renderer
-    SDL_Renderer* renderer = NULL;
-    renderer =  SDL_CreateRenderer( window, -1, SDL_RENDERER_ACCELERATED);
+complex<double> J(0.0, 1.0);
+int SKIPS = HEIGHT / STRIPS;
 
-    surface = IMG_ReadXPMFromArray(icon_xpm);
-    texture = SDL_CreateTextureFromSurface(renderer, surface);
-    if (!texture) {
-        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION,
-                     "Couldn't load texture: %s", SDL_GetError());
-        return 4;
-    }
+char* GetAbsCwdPath()
+{
+	#ifndef WIN32
+	// Linux And Android
+	char abs_cwd_path[PATH_MAX];
+	realpath("./", abs_cwd_path);
+	#else
+	// Windows (idk why)
+	//char abs_cwd_path[MAX_PATH];
+	#endif
+	
+	return abs_cwd_path;
+}
 
-    // Set render color to red ( background will be rendered in this color )
-    SDL_SetRenderDrawColor( renderer, 220, 220, 220, 255 );
+char* GetUniqueName()
+{
+	time_t raw_time = time(NULL);
+	struct tm* time_now = localtime(&raw_time);
 
-    // Clear winow
-    SDL_RenderClear( renderer );
+	char unik_name[50] = "";
+	strcat(unik_name, "./Shots/");
+	strcat(unik_name, "Shot ");
+	strcat(unik_name, asctime(time_now)); unik_name[strlen(unik_name) - 6] = '\0';
+	strcat(unik_name, ".bmp");
 
-    // Display image
-    SDL_Rect dstrect;
+	for (int i = 0; unik_name[i]; i++)
+	{
+		if (unik_name[i] == ' ')
+		{
+			unik_name[i] = '_';
+		}
+		else if (unik_name[i] == ':')
+		{
+			unik_name[i] = '-';
+		}
+	}
+	
+	/*
+	// Checking if File Already Exists
+	if (FILE* f = fopen(unik_name, "rb"))
+	{
+		fclose(f);
+		strcat(unik_name, "_");
+	}
+	else
+	{
+		strcat(unik_name, "_a");
+	}
+	*/
+	return unik_name;
+}
 
+void sdl_draw_mandelbrot(SDL_Window* window, SDL_Surface* surface, int CenterX, int CenterY, double zoom)
+{
+	int f, x, y, n;
+	int maxiter = (WIDTH / 2) * DETAILS * log10(zoom);
+	complex<double> z, c;
+	float C;
 
-    // Load smiley.png and display it.
-    // Location of image files for Android is: app/src/main/assets
-    SDL_Surface *loadedSurface = IMG_Load("smiley.png");
-    if (!loadedSurface) {
-        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION,
-                     "Couldn't load PNG image: %s", SDL_GetError());
-        return 5;
-    }
+	fprintf(stderr, "zoom: %f\n", zoom);
+	fprintf(stderr, "center point: %d, %d\n", CenterX, CenterY);
+	fprintf(stderr, "iterations: %d\n", maxiter);
 
-    SDL_Surface *backgroundSurface = IMG_Load("brno-snow.jpg");
-    if (!backgroundSurface) {
-        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION,
-                     "Couldn't load JPG image: %s", SDL_GetError());
-        return 6;
-    }
+	for (f = 0; f < SKIPS; f++)
+	{
+		#ifdef STRIP_BY_STRIP
+		for (y = SKIPS*f; y < SKIPS*(f+1); y++)		// Top to Bottom
+		#else
+		for (y = f; y < HEIGHT; y += SKIPS)		// Top to Bottom
+		#endif
+		{
+			for (x = 0; x < WIDTH; x++)			// Left to Right
+			{
+				// Get the complex point on gauss space to be calculate
+				z = c = CenterX + ((x - (WIDTH / 2)) / zoom) +
+					((CenterY + ((y - (HEIGHT / 2)) / zoom)) * J);
 
-    // Initialize TTF
-    if (TTF_Init() == -1) {
-        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "TTF_Init: %s\n", TTF_GetError());
-        return 7;
-    }
+				#define X real(z)
+				#define Y imag(z)
+				
+				// Check if point lies within the main cardiod or in the period-2 buld
+				if ((pow(X-0.25, 2)+pow(Y, 2))*(pow(X, 2)+(X / 2)+pow(Y, 2)-0.1875) < pow(Y, 2)/4 || pow(X+1, 2)+pow(Y, 2) < 0.0625)
+					n = maxiter;
+				else
+					/* Applies the actual mandelbrot formula on that point */
+					for (n = 0; n <= maxiter && abs(z) < BAIL_OUT; n++)
+						z = pow(z, SEED) + c;
 
-    // Load font
-    TTF_Font *font = TTF_OpenFont("blazed.ttf", 32);
-    if (!font) {
-        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION,
-                     "Unable to load font: %s\n", TTF_GetError());
-        return 8;
-    }
+				C = n - log2f(logf(abs(z)) / M_LN2);
 
-    dstrect.x = 0;
-    dstrect.y = 0;
-    dstrect.w = backgroundSurface->w;
-    dstrect.h = backgroundSurface->h;
-    SDL_Texture *backgroundTexture = SDL_CreateTextureFromSurface(renderer, backgroundSurface);
-    SDL_RenderCopy(renderer, backgroundTexture, NULL, &dstrect);
+				// Paint the pixel calculated depending on the number of iterations found
+				((Uint32*)surface->pixels)[(y * surface->w) + x] = (n >= maxiter) ? 0 :
+					SDL_MapRGB(surface->format,
+						(1 + sin(C * 0.27 + 5)) * 127., (1 + cos(C * 0.85)) * 127., (1 + sin(C * 0.15)) * 127.);
+			}
+		}
 
-    dstrect.x = 190;
-    dstrect.y = 130;
-    dstrect.w = loadedSurface->w;
-    dstrect.h = loadedSurface->h;
-    SDL_Texture *smileyTexture = SDL_CreateTextureFromSurface(renderer, loadedSurface);
-    SDL_RenderCopy(renderer, smileyTexture, NULL, &dstrect);
+		SDL_UpdateWindowSurface(window);
+	}
+}
 
-    dstrect.x = 20;
-    dstrect.y = 120;
-    dstrect.w = 128;
-    dstrect.h = 128;
-    SDL_RenderCopy(renderer, texture, NULL, &dstrect);
+int main()
+{
+	#ifdef _unix_
+	freopen("output.txt", "w", stderr);
+	#endif
+	if (SDL_Init(SDL_INIT_VIDEO) < 0)
+	{
+		fprintf(stderr, "Could not initialize SDL: %s\n", SDL_GetError());
+		exit(1);
+	}
+	atexit(SDL_Quit);
 
-    dstrect.x = 0;
-    dstrect.y = 0;
-    dstrect.w = 450;
-    dstrect.h = 100;
-    SDL_Color textColor = { 255, 240, 0, 255 };
-    SDL_Surface* solid = TTF_RenderText_Solid(font, "SDL2 Android Example", textColor);
+	SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "2");
+	SDL_Window* window;
 
-    SDL_Texture* solidTexture = SDL_CreateTextureFromSurface(renderer, solid);
-    SDL_RenderCopy(renderer, solidTexture, NULL, &dstrect);
-    SDL_FreeSurface(solid);
+	window = SDL_CreateWindow("SDL Mandelbrot",
+		SDL_WINDOWPOS_UNDEFINED,
+		SDL_WINDOWPOS_UNDEFINED,
+		WIDTH,
+		HEIGHT,
+		SDL_WINDOW_OPENGL
+	);
 
-    // SDL2_gfx example
-    thickLineColor(renderer, 0, 300, 300, 300, 20, 0xFF00FFFF) ;
+	SDL_Surface* surface = SDL_GetWindowSurface(window);
 
-    // Render to the screen
-    SDL_RenderPresent(renderer);
+	/* Initialize variables */
+	int CenterX = START_POS_X;
+	int CenterY = START_POS_Y;
+	double zoom = START_ZOOM;
 
-    // Event loop
-    bool quit = false;
-    SDL_Event event;
-    while(!quit && SDL_WaitEvent(&event)) {
-        switch (event.type) {
-            case SDL_QUIT:
-                quit = true;
-                break;
-            case SDL_KEYDOWN:
-                if ((event.key.keysym.sym == SDLK_AC_BACK) || (event.key.keysym.sym == SDLK_ESCAPE)) {
-                    quit = true;
-                }
-                break;
+	sdl_draw_mandelbrot(window, surface, CenterX, CenterY, zoom);
 
-            case SDL_FINGERDOWN: {
-                Mix_PlayChannel(-1, sample, 0);
-                dstrect.x = event.tfinger.x * loadedSurface->w;
-                dstrect.y = event.tfinger.y * loadedSurface->h;
-                dstrect.w = 128;
-                dstrect.h = 128;
-                SDL_RenderCopy(renderer, texture, NULL, &dstrect);
-                SDL_RenderPresent(renderer);
-                break;
-            }
-            default:
-                break;
-        }
-    }
+	SDL_Event event;
+	while (1)
+	{
+		SDL_PollEvent(&event);
+		switch (event.type)
+		{
+		case SDL_QUIT:
+			exit(0);
+			break;
 
-    Mix_CloseAudio();
+		case SDL_KEYDOWN:
+			if (event.key.keysym.sym == SDLK_TAB)
+			{
+				CenterX = START_POS_X;
+				CenterY = START_POS_Y;
+				zoom = START_ZOOM;
+				sdl_draw_mandelbrot(window, surface, CenterX, CenterY, zoom);
+			}
+			else if (event.key.keysym.sym == SDLK_ESCAPE)
+			{
+				exit(0);
+			}
+			else if (event.key.keysym.sym == SDLK_SPACE)
+			{
+				//SDL_RenderReadPixels(renderer, NULL, SDL_PIXELFORMAT_ARGB8888, surface->pixels, surface->pitch);
+				if (SDL_SaveBMP(surface, GetUniqueName()) != 0)
+					fprintf(stderr, "Save BMP Failed : %s\n", SDL_GetError());
+				SDL_FreeSurface(surface);
+			}
+			break;
 
-    // Close and destroy the window
-    SDL_DestroyWindow(window);
+		case SDL_MOUSEBUTTONDOWN:
 
-    // Clean up
-    SDL_Quit();
+			CenterX = (CenterX + (event.button.x - (WIDTH / 2))) / (zoom/ZOOM_FACTOR);
+			CenterY = (CenterY + (event.button.y - (HEIGHT / 2))) / (zoom/ZOOM_FACTOR);
 
-    return 0;
+			fprintf(stderr, "\nMouse Click : %d, %d\n", event.button.x, event.button.y);
+			fprintf(stderr, "Mouse Diff : %d, %d\n\n", (WIDTH / 2) - event.button.x, (HEIGHT / 2) - event.button.y);
+
+			if (event.button.button == 1)
+				zoom *= ZOOM_FACTOR;
+			else if (event.button.button == 3)
+				zoom /= ZOOM_FACTOR;
+				
+			if (SDL_SaveBMP(surface, GetUniqueName()) != 0)
+				fprintf(stderr, "Save BMP Failed : %s\n", SDL_GetError());
+			SDL_FreeSurface(surface);
+
+			sdl_draw_mandelbrot(window, surface, CenterX, CenterY, zoom);
+			break;
+		}
+	}
+
+	SDL_DestroyWindow(window);
+	SDL_Quit();
+
+	return 0;
 }
